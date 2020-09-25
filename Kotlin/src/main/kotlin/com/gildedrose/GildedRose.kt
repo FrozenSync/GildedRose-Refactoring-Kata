6b27e5a9cdb509b96df6f233a4b6d23526db70f4
leroy <leroytruong@gmail.com>
2020-09-25 11:41:15 +0200

    refactor: simplify if statement

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -8,28 +8,19 @@ class GildedRose(var items: Array<Item>) {
                 continue
             }
 
-            if (item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert") {
-                if (item.quality > 0) {
-                    item.quality = item.quality - 1
-                }
-            } else {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1
+            when (item.name) {
+                "Aged Brie" -> if (item.quality < 50) item.quality += 1
+                "Backstage passes to a TAFKAL80ETC concert" -> {
+                    if (item.quality < 50) item.quality += 1
 
-                    if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
-                        if (item.sellIn < 11) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1
-                            }
-                        }
-
-                        if (item.sellIn < 6) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1
-                            }
-                        }
+                    if (item.sellIn < 11) {
+                        if (item.quality < 50) item.quality += 1
+                    }
+                    if (item.sellIn < 6) {
+                        if (item.quality < 50) item.quality += 1
                     }
                 }
+                else -> if (item.quality > 0) item.quality -= 1
             }
 
             item.sellIn = item.sellIn - 1
