bd18a601e911f2abea2a0cea5978a1efb63bee38
leroy <leroytruong@gmail.com>
2020-09-25 11:21:35 +0200

    refactor: simplify if statement
    Add a test for the edge case of Aged Brie decreasing in quality. This can happen if the original branch structure is not honoured during refactoring.

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -35,18 +35,10 @@ class GildedRose(var items: Array<Item>) {
             item.sellIn = item.sellIn - 1
 
             if (item.sellIn < 0) {
-                if (item.name != "Aged Brie") {
-                    if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
-                        if (item.quality > 0) {
-                            item.quality = item.quality - 1
-                        }
-                    } else {
-                        item.quality = item.quality - item.quality
-                    }
-                } else {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1
-                    }
+                when (item.name) {
+                    "Aged Brie" -> if (item.quality < 50) item.quality += 1
+                    "Backstage passes to a TAFKAL80ETC concert" -> item.quality = 0
+                    else -> if (item.quality > 0) item.quality -= 1
                 }
             }
         }
