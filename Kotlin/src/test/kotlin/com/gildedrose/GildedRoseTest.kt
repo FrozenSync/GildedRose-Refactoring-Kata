43b272366e1f43f3c7d354e8dfd0dd23653e1b21
leroy <leroytruong@gmail.com>
2020-09-25 10:43:29 +0200

    test: add tests derived from specification

--- Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
+++ Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -6,12 +6,151 @@ import org.junit.jupiter.api.Test
 internal class GildedRoseTest {
 
     @Test
-    fun foo() {
+    fun `Name of an item remains the same after an update`() {
         val items = arrayOf(Item("foo", 0, 0))
-        val app = GildedRose(items)
+        val subject = GildedRose(items)
 
-        app.updateQuality()
+        subject.updateQuality()
+        val result = subject.items[0]
 
-        assertEquals("foo", app.items[0].name)
+        assertEquals("foo", result.name)
+    }
+
+    @Test
+    fun `Quality of an item degrades each passing day`() {
+        val items = arrayOf(Item("foo", 69, 420))
+        val subject = GildedRose(items)
+
+        subject.updateQuality()
+        val result = subject.items[0]
+
+        assertEquals(68, result.sellIn)
+        assertEquals(419, result.quality)
+    }
+
+    @Test
+    fun `Quality of an item degrades twice as fast once the sell by date has passed`() {
+        val items = arrayOf(Item("foo", 0, 420))
+        val subject = GildedRose(items)
+
+        subject.updateQuality()
+        val result = subject.items[0]
+
+        assertEquals(-1, result.sellIn)
+        assertEquals(418, result.quality)
+    }
+
+    @Test
+    fun `Quality of an item is never negative`() {
+        val items = arrayOf(Item("foo", 0, 0))
+        val subject = GildedRose(items)
+
+        subject.updateQuality()
+        val result = subject.items[0]
+
+        assertEquals(-1, result.sellIn)
+        assertEquals(0, result.quality)
+    }
+
+    @Test
+    fun `Quality of "Aged Brie" increases the older it gets`() {
+        val items = arrayOf(Item("Aged Brie", 6, 6))
+        val subject = GildedRose(items)
+
+        subject.updateQuality()
+        val result = subject.items[0]
+
+        assertEquals(5, result.sellIn)
+        assertEquals(7, result.quality)
+    }
+
+    @Test
+    fun `Quality of "Aged Brie" increases twice as fast past sellIn date`() {
+        val items = arrayOf(Item("Aged Brie", 0, 6))
+        val subject = GildedRose(items)
+
+        subject.updateQuality()
+        val result = subject.items[0]
+
+        assertEquals(-1, result.sellIn)
+        assertEquals(8, result.quality)
+    }
+
+    @Test
+    fun `Quality of "Backstage passes" increases the older it gets`() {
+        val items = arrayOf(Item("Backstage passes to a TAFKAL80ETC concert", 66, 0))
+        val subject = GildedRose(items)
+
+        subject.updateQuality()
+        val result = subject.items[0]
+
+        assertEquals(65, result.sellIn)
+        assertEquals(1, result.quality)
+    }
+
+    @Test
+    fun `Quality of "Backstage passes" increases by 2 when there are 10 days or less`() {
+        val items = arrayOf(Item("Backstage passes to a TAFKAL80ETC concert", 10, 33))
+        val subject = GildedRose(items)
+
+        subject.updateQuality()
+        val result = subject.items[0]
+
+        assertEquals(9, result.sellIn)
+        assertEquals(35, result.quality)
+    }
+
+    @Test
+    fun `Quality of "Backstage passes" increases by 3 when there are 5 days or less`() {
+        val items = arrayOf(Item("Backstage passes to a TAFKAL80ETC concert", 5, 33))
+        val subject = GildedRose(items)
+
+        subject.updateQuality()
+        val result = subject.items[0]
+
+        assertEquals(4, result.sellIn)
+        assertEquals(36, result.quality)
+    }
+
+    @Test
+    fun `Quality of "Backstage passes" drops to 0 after the concert`() {
+        val items = arrayOf(Item("Backstage passes to a TAFKAL80ETC concert", 0, 50))
+        val subject = GildedRose(items)
+
+        subject.updateQuality()
+        val result = subject.items[0]
+
+        assertEquals(-1, result.sellIn)
+        assertEquals(0, result.quality)
+    }
+
+    @Test
+    fun `Quality of an item is never increases beyond 50`() {
+        val items = arrayOf(
+                Item("Aged Brie", 66, 50),
+                Item("Backstage passes to a TAFKAL80ETC concert", 66, 50)
+        )
+        val subject = GildedRose(items)
+
+        subject.updateQuality()
+        val result = subject.items
+
+        assertEquals(65, result[0].sellIn)
+        assertEquals(50, result[0].quality)
+
+        assertEquals(65, result[1].sellIn)
+        assertEquals(50, result[1].quality)
+    }
+
+    @Test
+    fun `Quality of "Sulfuras" is always 80 and never has to be sold`() {
+        val items = arrayOf(Item("Sulfuras, Hand of Ragnaros", 666, 80))
+        val subject = GildedRose(items)
+
+        subject.updateQuality()
+        val result = subject.items[0]
+        
+        assertEquals(666, result.sellIn)
+        assertEquals(80, result.quality)
     }
 }
